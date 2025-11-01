3597f2b246fa6b58469a018cb9b20fddc32064e5
zhsyoo <zhsyoo@gmail.com>
2025-11-01 16:16:38 +0900

    separate service and web ans gateway of docker-compose.yml

--- run_dev.sh
+++ run_dev.sh
@@ -44,13 +44,20 @@ check_error() {
 #docker compose --env-file .env.dev -f docker-compose-infra.yml up -d
 
 #####DOCKER_BUILDKIT=0 
-#docker compose --env-file .env.dev -f docker-compose-service.yml build # --progress=plain # --no-cache
-#check_error
-#docker compose --env-file .env.dev -f docker-compose-service.yml down
-#docker compose --env-file .env.dev -f docker-compose-service.yml up -d
+docker compose --env-file .env.dev -f docker-compose-service.yml build # --progress=plain  --no-cache
+check_error
+docker compose --env-file .env.dev -f docker-compose-service.yml down
+docker compose --env-file .env.dev -f docker-compose-service.yml up -d
 
 #####DOCKER_BUILDKIT=0 
-docker compose --env-file .env.dev -f docker-compose-web.yml build # --progress=plain # --no-cache
+docker compose --env-file .env.dev -f docker-compose-web.yml build #  --progress=plain  --no-cache
 check_error
 docker compose --env-file .env.dev -f docker-compose-web.yml down
 docker compose --env-file .env.dev -f docker-compose-web.yml up -d
+
+#####DOCKER_BUILDKIT=0 
+docker compose --env-file .env.dev -f docker-compose-gateway.yml build #  --progress=plain  --no-cache
+check_error
+docker compose --env-file .env.dev -f docker-compose-gateway.yml down
+docker compose --env-file .env.dev -f docker-compose-gateway.yml up -d
+
